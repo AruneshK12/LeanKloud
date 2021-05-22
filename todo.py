fa2dc40b71d83778d5d34205eb383438354c8f2d
Arunesh Kumar <34567890+AruneshK12@users.noreply.github.com>
2021-05-22 14:11:44 +0530

    Updated TodoMVC with Token based Authentication

--- todo.py
+++ todo.py
@@ -1,15 +1,67 @@
 from flask import Flask,request
 from flask_restplus import Api, Resource, fields
 from werkzeug.middleware.proxy_fix import ProxyFix
+from functools import wraps
 import mysql.connector
 
 mydb=mysql.connector.connect(host="localhost",username="root",password="ADMIN",database="test")
 
+
+auth_types={
+    'AccessKey':{
+        'type':'apiKey',
+        'in':'header',
+        'name':'Task-Key'
+    }
+}
 app = Flask(__name__)
+
 app.wsgi_app = ProxyFix(app.wsgi_app)
-api = Api(app, version='1.0', title='TodoMVC API',
-    description='A simple TodoMVC API',
-)
+api = Api(app, version='1.0', title='TodoMVC API',description='A simple TodoMVC API',authorizations=auth_types)
+
+def token_required_write(f):
+    @wraps(f)
+    def decorated(*args,**kwargs):
+        token= None
+        check=0
+        if 'Task-Key' in request.headers:
+            token=request.headers['Task-Key']
+            myc2=mydb.cursor()
+            query="Select * from AcessKeys where KeyName=\""+str(token)+"\""
+            myc2.execute(query)
+            token_det=myc2.fetchall()
+            for row in token_det:
+                check=1
+                if(row[1]==0):
+                    return {'message':'Invalid Write-Key, Operation Denied'},401
+        if(check==0):
+            return {'message':'Access Key Doesnt Exist, Operation Deined'},401
+        if not token:
+            return {'message':'Missing Access Key,Operation Terminated'},401
+        return f(*args,**kwargs)
+    return decorated
+
+def token_required_read(f):
+    @wraps(f)
+    def decorated(*args,**kwargs):
+        token= None
+        check=0
+        if 'Task-Key' in request.headers:
+            token=request.headers['Task-Key']
+            myc2=mydb.cursor()
+            query="Select * from AcessKeys where KeyName=\""+str(token)+"\""
+            myc2.execute(query)
+            token_det=myc2.fetchall()
+            for row in token_det:
+                check=1
+                if(row[1]==1):
+                    return {'message':'Invalid Read-Key, Operation Denied'},401
+        if(check==0):
+            return {'message':'Access Key Doesnt Exist, Operation Deined'},401
+        if not token:
+            return {'message':'Missing Access Key,Operation Terminated'},401
+        return f(*args,**kwargs)
+    return decorated
 
 ns = api.namespace('todos', description='TODO operations')
 
@@ -155,13 +207,15 @@ DAO = TodoDAO()
 @ns.route('/')
 class TodoList(Resource):
     '''Shows a list of all todos, and lets you POST to add new tasks'''
-    @ns.doc('list_todos')
+    @ns.doc('list_todos',security='AccessKey')
+    @token_required_read
     @ns.marshal_list_with(todo)
     def get(self):
         '''List all tasks'''
         return DAO.getAll()
 
-    @ns.doc('create_todo')
+    @ns.doc('create_todo',security='AccessKey')
+    @token_required_write
     @ns.expect(todo)
     @ns.marshal_with(todo, code=201)
     def post(self):
@@ -174,19 +228,23 @@ class TodoList(Resource):
 @ns.param('id', 'The task identifier')
 class Todo(Resource):
     '''Show a single todo item and lets you delete them'''
-    @ns.doc('get_todo')
+    @ns.doc('get_todo',security='AccessKey')
+    @token_required_read
     @ns.marshal_with(todo)
     def get(self, id):
         '''Fetch a given resource'''
         return DAO.get(id)
 
-    @ns.doc('delete_todo')
+    @ns.doc('delete_todo',security="AccessKey")
+    @token_required_write
     @ns.response(204, 'Todo deleted')
     def delete(self, id):
         '''Delete a task given its identifier'''
         DAO.delete(id)
         return '', 204
 
+    @ns.doc('Update_todo',security="AccessKey")
+    @token_required_write
     @ns.expect(todo)
     @ns.marshal_with(todo)
     def put(self, id):
@@ -200,6 +258,8 @@ class Todo(Resource):
 @ns.param('id','The task identifier')
 class TodoStatus(Resource):
     '''to change the status of the resource'''
+    @ns.doc('Status_Updation',security="AccessKey")
+    @token_required_write
     @ns.expect(todo_status)
     @ns.marshal_with(todo_status)
     def put(self,id):
@@ -210,6 +270,8 @@ class TodoStatus(Resource):
 @ns.response(404,'Todo Not Found')
 class TodoDueDate(Resource):
     '''to check due tasks'''
+    @ns.doc('View_Due_Tasks',security="AccessKey")
+    @token_required_read
     def get(self):
         '''enter Due date in YYYY-MM-DD format to get current due tasks'''
         due_date=request.args.get("due_date")
@@ -219,6 +281,8 @@ class TodoDueDate(Resource):
 @ns.response(404,'Todo Task not found')
 class todoOverDue(Resource):
     '''to get overdue tasks'''
+    @ns.doc('Get_OverDue_Tasks',security="AccessKey")
+    @token_required_read
     def get(self):
         '''to get the overdue tasks in the todoList'''
         return DAO.overdueTasks()
@@ -227,6 +291,8 @@ class todoOverDue(Resource):
 @ns.response(404,'Todo Task not found')
 class todoFinished(Resource):
     '''to get the finished tasks'''
+    @ns.doc('Get_Finished_Tasks',security="AccessKey")
+    @token_required_read
     def get(self):
         '''GET request to get all finished tasks'''
         return DAO.finished()
